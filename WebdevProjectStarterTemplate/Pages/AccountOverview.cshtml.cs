175506faaa0ba35a9ea16d4336bc5d6b256dbfa8
Hindrik Sjieuwe de Hoop <114724216+HSdeH@users.noreply.github.com>
2023-03-28 22:05:30 +0200

    table select klaar

--- WebdevProjectStarterTemplate/Pages/AccountOverview.cshtml.cs
+++ WebdevProjectStarterTemplate/Pages/AccountOverview.cshtml.cs
@@ -11,21 +11,31 @@ namespace WebdevProjectStarterTemplate.Pages;
 public class overviewModel : PageModel
 {
     public CafeUser user { get; set; }
-    public string id { get; set; }
+    public string userid { get; set; }
     public List<Table> tables { get; set; }
+    //[BindProperty(RTableId = "tableselect")]
+
+    public string RTableId { get; set; } = "nietveranderd";
     public IActionResult OnGet()
     {
-        id = HttpContext.Session.GetString("userID");
-        if (id == null )
+        userid = HttpContext.Session.GetString("userID");
+        if (userid == null )
         {
             return new RedirectResult("login");
         }
         //Guid guid = Guid.Parse(id);
 
-        user = new UserRepo().GetUser(Convert.ToInt32(id));
+        user = new UserRepo().GetUser(Convert.ToInt32(userid));
 
         tables = new MiscRepo().GetTables();
 
+        return Page();
+    }
+    public IActionResult OnPost()
+    {
+        RTableId = Request.Form["tableselect"]; //weet niet waarom, maar anders bind hij niet
+        TableSelector(Convert.ToInt32(RTableId));
+        OnGet();            //TODO misschien nog iets met [viewdata] om te laten zien dat je daadwerkelijk een tafel selecteert // OnGet omdat hij anders tables kwijtraakt
         return Page();
     }
     public void TableSelector(int RTableId)
