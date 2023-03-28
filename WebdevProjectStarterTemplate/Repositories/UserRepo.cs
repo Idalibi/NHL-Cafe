b50fef9d174443d4e8e6f01afd20ef982b5c5ccf
Hindrik Sjieuwe de Hoop <114724216+HSdeH@users.noreply.github.com>
2023-03-28 14:57:22 +0200

    userrepo compleet

--- WebdevProjectStarterTemplate/Repositories/UserRepo.cs
+++ WebdevProjectStarterTemplate/Repositories/UserRepo.cs
@@ -15,9 +15,24 @@ namespace WebdevProjectStarterTemplate.Repositories
         public CafeUser AddUser(CafeUser CafeUser) //register/sign up
         {
             string sql = @" INSERT INTO CafeUser (UserName, Password) VALUES (@UserName, @Password);
-                            SELECT * FROM CafeUser WHERE UniqueId = LAST_INSERT_ID()";
+                            SELECT * FROM CafeUser WHERE UniqueGuid = LAST_INSERT_ID()";
             var connection = GetConnection();
-            var returnuser = connection.QuerySingleOrDefault<CafeUser>(sql, CafeUser);//zou moeten falen als niet uniek
+            var returnuser = connection.QuerySingleOrDefault<CafeUser>(sql, CafeUser);//zou moeten falen(of defaulten?) als naam niet uniek
+            return returnuser;
+        }
+
+        public CafeUser GetUser(CafeUser CafeUser)//login
+        {
+            string sql = @" SELECT * FROM CafeUser WHERE UserName = @UserName AND Password = @Password;";
+            var connection = GetConnection();
+            var returnuser = connection.QuerySingleOrDefault<CafeUser> (sql, CafeUser);
+            return returnuser;
+        }
+        public CafeUser GetUser(int id)//ophalen van gegevens tijdens sessie
+        {
+            string sql = @"SELECT * WHERE UniqueGuid = @ID";
+            var connection = GetConnection();
+            var returnuser = connection.QuerySingleOrDefault<CafeUser>(sql, new {ID =id});
             return returnuser;
         }
     }
