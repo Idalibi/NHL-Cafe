ddc818ad9f65d8f6d2cf6ab3412b36b06d53c070
Hindrik Sjieuwe de Hoop <114724216+HSdeH@users.noreply.github.com>
2023-03-28 18:17:40 +0200

    paginas toegevoegd

---
+++ WebdevProjectStarterTemplate/Pages/Rest/Alles betalen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebdevProjectStarterTemplate.Pages.Rest
+{
+    public class Alles_betalenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebdevProjectStarterTemplate/Pages/Rest/Overzicht.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebdevProjectStarterTemplate.Pages.Rest
+{
+    public class OverzichtModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
